ee8359727ef20666ad8a53262d374daf42ff64d6
mazurbeam <mazurbeam@gmail.com>
2017-12-06 19:18:27 -0800

    connected to hackernews API to retrieve stories

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  }, 
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }, 
-];
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'http://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
 const isSearched = searchTerm => item => 
     item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -30,12 +19,30 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: '',
-    }
+      result: null,
+      searchTerm: DEFAULT_QUERY,
+    };
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({result});
+  }
+
+  fetchSearchTopStories(searchTerm) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(e => e);
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
   }
 
   onSearchChange(event) {
@@ -49,7 +56,10 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) {return null; }
+
     return (
       <div className="page">
         <div className="interactions">
@@ -59,7 +69,7 @@ class App extends Component {
           > Search</Search>
         </div>
         <Table 
-          list = {list}
+          list = {result.hits}
           pattern = {searchTerm}
           onDismiss = {this.onDismiss}
         />
@@ -68,6 +78,10 @@ class App extends Component {
     );
   }
 }
+
+const largeColumn = { width: '40%' }
+const midColumn = { width: '30%' }
+const smallColumn = { width: '10%' }
 
 const Search = ({value, onChange, children }) =>
     <form>
@@ -82,10 +96,10 @@ const Table = ({list, pattern, onDismiss}) =>
   <div className="table">
   {list.filter(isSearched(pattern)).map(item => 
     <div key={item.objectID} className="table-row">
-      <span style={{width: '40%'}}><a href={item.url}>{item.title}</a></span>
-      <span style={{width: '30%'}}>{item.author}</span>
-      <span style={{width: '10%'}}>{item.num_comments}</span>
-      <span style={{width: '10%'}}>{item.points}</span>
+      <span style={largeColumn}><a href={item.url}>{item.title}</a></span>
+      <span style={midColumn}>{item.author}</span>
+      <span style={smallColumn}>{item.num_comments}</span>
+      <span style={smallColumn}>{item.points}</span>
       <span>
         <Button onClick={()=> onDismiss(item.objectID)} className="button-inline">Dismiss</Button>
       </span>
